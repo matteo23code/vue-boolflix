18eaf7c3f974f5260cba924424b0591d8f8b080f
Matteo Basile <matteo.basile56@gmail.com>
2021-02-10 17:39:01 +0100

    aggiunta numero intero e voto con stelle

--- js/script.js
+++ js/script.js
@@ -4,22 +4,27 @@ new Vue({
     movie: '',
      movieList:[
 
+    ],
+    flagList:[
+
     ]
   },
   methods:{
     search: function () {
       const self = this;
-
-      axios.get('https://api.themoviedb.org/3/search/movie?api_key=883ce966ab68df0a28febc7e02e61163&query=' + this.movie)
-
-        .then(function(resp) {
-          const film = resp.data.results
+      
+      axios.get('https://api.themoviedb.org/3/search/movie?api_key=883ce966ab68df0a28febc7e02e61163&language=it IT&query=' + this.movie)
+      
+      .then((resp) => {
+          const film = resp.data.results;
           console.log(resp.data.results);
-         self.movieList = film;
-      });
-
-    }
-  },
+          self.movieList = film;
+        });
+    },
+    vote:(vote) => {
+      return parseInt(vote / 2)
+    },
+  }
 
 });
 Vue.config.devtools = true
